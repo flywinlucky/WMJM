d43a65c6966e96f8123529a9f420be20607eeab6
Traffic Wolf <flywinlucky777@gmail.com>
2025-04-23 22:36:31 +0300

    g

--- Assets/InterstitialYG.cs
+++ Assets/InterstitialYG.cs
@@ -5,10 +5,7 @@ using System.Collections;
 
 public class InterstitialYG : MonoBehaviour
 {
-    public TMP_Text countdownInterText; // Textul pentru countdown
-    public GameObject interstitialAdAlert; // Obiectul pentru reclama interstitială  
-
-    public float countdownTimer = 65f; // Durata countdown-ului în secunde
+    public float countdownTimer; // Durata countdown-ului în secunde
     public bool enableInterstitials = true; // Starea countdown-ului
     private bool isShowingAd = false; // Flag pentru a preveni apeluri multiple
 
@@ -32,26 +29,10 @@ public class InterstitialYG : MonoBehaviour
         if (isShowingAd) yield break;
         isShowingAd = true;
 
-        // Activează UI-ul pentru countdown
-        interstitialAdAlert.SetActive(true);
-        countdownInterText.gameObject.SetActive(true);
-
-        // Numărătoare inversă simplă 3, 2, 1
-        countdownInterText.text = "3";
         yield return new WaitForSeconds(1f);
-
-        countdownInterText.text = "2";
-        yield return new WaitForSeconds(1f);
-
-        countdownInterText.text = "1";
-        yield return new WaitForSeconds(1f);
-
-        // Ascunde UI-ul
-        interstitialAdAlert.SetActive(false);
-        countdownInterText.gameObject.SetActive(false);
 
         // Afișează reclama interstitială
-        Debug.Log("Showing interstitial ad after countdown");
+        Debug.Log("Showing interstitial ad");
         YG2.InterstitialAdvShow();
 
         // Resetează timer-ul și starea
